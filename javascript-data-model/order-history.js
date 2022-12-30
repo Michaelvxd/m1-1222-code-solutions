cbf2988e101bdbe03f7aa48d31e48132938e1a36
Michael Van Doorn <md@clickup.com>
2022-12-30 01:09:01 +0000

    Javascript Data Model - Michael Van Doorn

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,31 @@
+var orderHistory = [
+  {
+    itemOne: 'JavaScript for impatient programmers',
+    author: 'Rauschmayer, Dr. Axel',
+    total: 34.00,
+    deliveryDate: 'August 4th, 2020'
+  },
+  {
+    itemOne: 'The Timeless Way of Building',
+    author: 'Alexander, Christopher',
+    total: 44.53,
+    orderPlaced: 'July 19th, 2020',
+    deliveryDate: 'July 20, 2020'
+  },
+  {
+    itemOne: 'Gamecube Controller adapter',
+    total: 17.22,
+    orderPlaced: 'July 4th, 2020',
+    deliveryDate: 'July 7th, 2020'
+  },
+  {
+    itemOne: 'Gamecube Controller - Super Smash Bros. Edition',
+    costOne: 94.95,
+    itemTwo: 'The Aray of Sql',
+    author: 'Faroult, Stephane',
+    costTwo: 94.95,
+    total: 138.93,
+    orderPlaced: 'July 3rd, 2020',
+    deliveryDate: 'July 5th, 2020'
+  }
+];
