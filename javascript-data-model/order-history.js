97b64cb73228ba1e803f0669f093ae75af6f8fc1
Michael Van Doorn <md@clickup.com>
2022-12-30 01:45:12 +0000

    Data Model Updates

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -1,31 +1,45 @@
 var orderHistory = [
   {
     itemOne: 'JavaScript for impatient programmers',
+    imgItemOne: './img/url',
     author: 'Rauschmayer, Dr. Axel',
     total: 34.00,
-    deliveryDate: 'August 4th, 2020'
+    deliveryDate: 'August 4th, 2020',
+    orderNumber: '114 - 3941689 - 8772232',
+    rtrnItemOne: 'Sept 7th, 2020'
   },
   {
     itemOne: 'The Timeless Way of Building',
+    imgItemOne: './img/url',
     author: 'Alexander, Christopher',
     total: 44.53,
     orderPlaced: 'July 19th, 2020',
-    deliveryDate: 'July 20, 2020'
+    deliveryDate: 'July 20, 2020',
+    orderNumber: '113-9984268-1280257',
+    rtrnItemOne: 'Aug 19th, 2020'
   },
   {
     itemOne: 'Gamecube Controller adapter',
+    imgItemOne: './img/url',
     total: 17.22,
     orderPlaced: 'July 4th, 2020',
-    deliveryDate: 'July 7th, 2020'
+    deliveryDate: 'July 7th, 2020',
+    orderNumber: '114-2875557-9059409',
+    rtrnItemOne: 'Aug 5th, 2020'
   },
   {
     itemOne: 'Gamecube Controller - Super Smash Bros. Edition',
+    imgItemOne: './img/url',
     costOne: 94.95,
+    rtrnItemOne: 'Aug 4th, 2020',
     itemTwo: 'The Aray of Sql',
+    rtrnItemTwo: 'Aug 4th, 2020',
+    imgItemTwo: './img/url',
     author: 'Faroult, Stephane',
     costTwo: 94.95,
     total: 138.93,
     orderPlaced: 'July 3rd, 2020',
-    deliveryDate: 'July 5th, 2020'
+    deliveryDate: 'July 5th, 2020',
+    orderNumber: '113-2883177-2648248'
   }
 ];
