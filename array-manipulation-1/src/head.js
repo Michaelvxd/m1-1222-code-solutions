79ebdb5e3a04aa4e94dd800ab4deaf5222f177f1
Michael Van Doorn <md@clickup.com>
2023-01-10 04:45:13 +0000

    Array Manipulation 1

---
+++ array-manipulation-1/src/head.js
@@ -0,0 +1,9 @@
+/* exported head */
+
+// takes in an array
+// using dot or bracket notation, targets the first element
+// returns the first element of the array
+
+function head(array) {
+  return array[0];
+}
