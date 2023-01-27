44ad0d99e3bd2ec2a4124e8610ccff8d9ac6012c
Michael Van Doorn <md@clickup.com>
2023-01-27 03:24:45 +0000

    JavaScript View Swapping - Michael Van Doorn

---
+++ javascript-view-swapping/main.js
@@ -0,0 +1,23 @@
+var tabContain = document.querySelector('.tab-container');
+var tabList = document.querySelectorAll('.tab');
+var viewList = document.querySelectorAll('.view');
+
+tabContain.addEventListener('click', function (event) {
+  if (event.target.matches('.tab')) {
+    for (var i = 0; i < tabList.length; i++) {
+      if (tabList[i] === event.target) {
+        tabList[i].classList.add('active');
+        var dataView = event.target.getAttribute('data-view');
+        for (var j = 0; j < viewList.length; j++) {
+          if (dataView === viewList[j].getAttribute('data-view')) {
+            viewList[j].classList.remove('hidden');
+          } else {
+            viewList[j].classList.add('hidden');
+          }
+        }
+      } else {
+        tabList[i].classList.remove('active');
+      }
+    }
+  }
+});
