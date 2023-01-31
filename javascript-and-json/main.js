aacc7206b05c2314f46d04858c13b46808f4a7b3
Michael Van Doorn <md@clickup.com>
2023-01-31 03:03:57 +0000

    JavaScript and JSON - Michael Van Doorn

--- javascript-and-json/main.js
+++ javascript-and-json/main.js
@@ -19,12 +19,12 @@ var myBooks = [
 console.log('Your array of books is: ', myBooks);
 console.log('Your list of books is of type: ', typeof myBooks);
 
-console.log(JSON.stringify(myBooks));
-console.log(typeof JSON.stringify(myBooks));
+console.log('myBooks stringified is: ', JSON.stringify(myBooks));
+console.log('myBooks stringified is type of: ', typeof JSON.stringify(myBooks));
 
 var myStudent = '[{"number_id":45234,"name":"Michael Van Doorn"}]';
 console.log('myStudent is:', myStudent);
 console.log('myStudent is of type:', typeof myStudent);
 
-console.log(JSON.parse(myStudent));
-console.log(typeof JSON.parse(myStudent));
+console.log('myStudent JSON parsed is: ', JSON.parse(myStudent));
+console.log('JSON Parse of myStudent is type of: ', typeof JSON.parse(myStudent));
