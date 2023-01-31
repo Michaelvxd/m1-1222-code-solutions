a4fffd1d483adcabc8ea05fd1a4bec66c92a5de8
Michael Van Doorn <md@clickup.com>
2023-01-31 03:01:06 +0000

    JavaScript and JSON - Michael Van Doorn

---
+++ javascript-and-json/main.js
@@ -0,0 +1,30 @@
+var myBooks = [
+  {
+    isbn: 142410314,
+    name: 'Charlie and the Chocolate Factory',
+    author: 'Roadl Dahl'
+  },
+  {
+    isbn: 64431789,
+    name: 'Where the Wild Things Are',
+    author: 'Maurine Sendak'
+  },
+  {
+    isbn: 9780439358071,
+    name: 'Harry Potter and the Order of the Phoenix',
+    author: 'J.K Rowling'
+  }
+];
+
+console.log('Your array of books is: ', myBooks);
+console.log('Your list of books is of type: ', typeof myBooks);
+
+console.log(JSON.stringify(myBooks));
+console.log(typeof JSON.stringify(myBooks));
+
+var myStudent = '[{"number_id":45234,"name":"Michael Van Doorn"}]';
+console.log('myStudent is:', myStudent);
+console.log('myStudent is of type:', typeof myStudent);
+
+console.log(JSON.parse(myStudent));
+console.log(typeof JSON.parse(myStudent));
